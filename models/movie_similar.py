0585d11db469aeb2520395dfb048d3940d4c2ebc
BEANyyy <yevin2001@gmail.com>
2022-09-28 20:20:23 +0900

    moive_similar 수정

--- models/movie_similar.py
+++ models/movie_similar.py
@@ -4,22 +4,22 @@ from db import db
 class MovieSimilarModel(db.Model):
     __tablename__ = 'movie_similar'
 
-    movie_id = db.Column(db.Integer, db.ForeignKey(
-        'movies.id'), primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey(
+        'books.id'), primary_key=True)
     movie_similar_id = db.Column(db.Integer, db.ForeignKey(
         'movies.id'), primary_key=True)
 
-    def __init__(self, movie_id, movie_similar_id):
-        self.movie_id = movie_id
+    def __init__(self, book_id, movie_similar_id):
+        self.book_id = book_id
         self.movie_similar_id = movie_similar_id
 
     def json(self):
-        return {'movie_id': self.movie_id, 'movie_similar_id': self.movie_similar_id}
+        return {'book_id': self.book_id, 'movie_similar_id': self.movie_similar_id}
 
     @classmethod
     def find_by_movie_id(cls, movie_id):
         """
-        movie id를 입력하면 해당 책과 유사한 책 전부 가져오기
+        book id를 입력하면 해당 책과 유사한 책 전부 가져오기
         """
         return cls.query.filter_by(movie_id=movie_id).all()
 
